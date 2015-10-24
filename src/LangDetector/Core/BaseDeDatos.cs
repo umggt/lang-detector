ddd58cc16eeb5b3705c4ffab796d768b7534c0e1
Miguel Román <miguelerm@gmail.com>
2015-10-24 09:37:57 -0600

    Comentarios

--- src/LangDetector/Core/BaseDeDatos.cs
+++ src/LangDetector/Core/BaseDeDatos.cs
@@ -6,24 +6,36 @@ using System.Threading.Tasks;
 
 namespace LangDetector.Core
 {
+    /// <summary>
+    /// Clase en la que se agregan las funcionalidades para administrar la base de datos.
+    /// </summary>
     class BaseDeDatos
     {
         private const string dbName = "datos.db";
         private const string connectionString = "Data Source=" + dbName;
 
+        /// <summary>
+        /// Crea la base de datos si no existe aún en la misma carpeta que la aplicación.
+        /// </summary>
         public static void CrearSiNoExiste()
         {
             if (!File.Exists(dbName))
             {
+                // Si no existe el archivo de base de datos, se crea
+                // ejecutando el script de base de datos ubicado en la carpeta Scripts\Creacion-de-base-de-datos.sql
                 using (var conexion = AbrirConexion())
                 {
                     var comando = conexion.CreateCommand();
-                    comando.CommandText = Resources.ObtenerTexto("LangDetector.Scripts.201510211259-Initial-Create.sql");
+                    comando.CommandText = Resources.ObtenerTexto("LangDetector.Scripts.Creacion-de-base-de-datos.sql");
                     comando.ExecuteNonQuery();
                 }
             }
         }
 
+        /// <summary>
+        /// Método que abre una conexión hacia la base de datos.
+        /// </summary>
+        /// <returns>Retorna el objeto de conexión que se puede usar para ejecutar consultas hacia la base de datos.</returns>
         public static IDbConnection AbrirConexion()
         {
             var conexion = new SQLiteConnection(connectionString);
@@ -31,6 +43,10 @@ namespace LangDetector.Core
             return conexion;
         }
 
+        /// <summary>
+        /// Método que abre una conexión hacia la base de datos de forma asincrona (mas eficiente que el método AbrirConexion para procesos asincronos).
+        /// </summary>
+        /// <returns>Retorna el objeto de conexión que se puede usar para ejecutar consultas hacia la base de datos.</returns>
         public static async Task<IDbConnection> AbrirConexionAsync()
         {
             var conexion = new SQLiteConnection(connectionString);
